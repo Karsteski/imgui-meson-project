8bd0781b4d96866ad151e23979e39d82164fbb82
Kareem Stephan Skinner <kastsk@gmail.com>
2021-03-06 08:15:40 -0500

    Updated project

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include "imgui_impl_opengl3.h"
 #include <stdio.h>
 
+#include "functions.h"
+
 // About Desktop OpenGL function loaders:
 //  Modern desktop OpenGL doesn't have a standard portable header file to load OpenGL function pointers.
 //  Helper libraries are often used for this purpose! Here we are supporting a few common ones (gl3w, glew, glad).
